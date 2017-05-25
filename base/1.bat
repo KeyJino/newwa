6a69509bc19dbeed3f08721b9a37c3ec0517f5a6
KeyJino <jeka.kova@mail.ru>
2017-05-26 00:01:36 +0300

    Initial commit

---
+++ base/1.bat
@@ -0,0 +1,8 @@
+cd C:\base\
+expand -n1,2,3,4 -v2
+copy C:\base\list_bigtick.tex C:\base\1495584354292.tex
+copy C:\base\list_ans.tex C:\base\1495584354292_ans.tex
+pdflatex C:\base\1495584354292.tex
+pdflatex C:\base\1495584354292_ans.tex
+copy c:\base\1495584354292.pdf C:\xtex\pdfs\
+copy c:\base\1495584354292_ans.pdf C:\xtex\pdfs\
